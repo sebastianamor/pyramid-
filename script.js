dcc8cf75b1045a483aafcaf172db46ef4e5fc935
¨Jane <smoreira11@hotmail.com>
2024-04-29 15:39:31 +0900

    js preogress

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 let character = 'Hello';
 let count = 8;
-let rows = ["Naomi", "Quincy", "CamperChan"];
+let rows = [];
+
+const call = padRow("CamperChan");
+console.log(call);
+
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
