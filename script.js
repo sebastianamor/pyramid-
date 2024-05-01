15216d69f403ca34dda16bbb9501dd131352ebc5
¨Jane <smoreira11@hotmail.com>
2024-05-01 11:55:01 +0900

    ready for now

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-let character = 'Hello';
-let count = 8;
-let rows = [];
- 
+const character = "#";
+const count = 8;
+const rows = [];
+let inverted = true;
 
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-// TODO: use a different type of loop
-/*for (let i = 1; i <= count; i++) {
-  rows.push(padRow(i, count));
-}*/
-
-let continueLoop = false;
-let done = 0;
-
-while (continueLoop) {
-  done++; 
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
 }
 
 let result = ""
